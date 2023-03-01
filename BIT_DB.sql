8fa46afb26579eeec7d393b4a703158cbafd24ff
ynohtnaMac <125811063+ynohtnaMac@users.noreply.github.com>
2023-03-01 17:22:58 -0600

    Delete BIT_DB.sql

--- BIT_DB.sql
+++
@@ -1,8 +0,0 @@
-SELECT count(distinct cust.acctnum),
-avg(quantity*price)
-FROM BIT_DB.FebSales feb
-LEFT JOIN BIT_DB.customers cust
-ON feb.orderID=cust.order_id
-WHERE feb.Quantity > 2
-AND length(orderid) =6
-AND orderid <> 'Order ID'
